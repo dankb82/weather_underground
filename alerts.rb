d658c0d56073bb58da64bf4d906b4ee6df004687
Daniel Buompane <danielbuompane@gmail.com>
2016-02-22 15:00:39 -0500

    Initialize Alerts class

--- alerts.rb
+++ alerts.rb
@@ -1,3 +1,6 @@
 class Alerts
-  
+  attr_reader :alerts
+  def initialize(zipcode)
+    @alerts = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/alerts/q/#{zipcode}.json")
+  end
 end
