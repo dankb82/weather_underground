38be1ef1ebc11f47c3e8f1381e490a720c766b1a
Daniel Buompane <danielbuompane@gmail.com>
2016-02-22 14:01:00 -0500

    Create classes

---
+++ ten_day_forecast.rb
@@ -0,0 +1,2 @@
+class TenDayForecast
+end
--- tests.rb
+++ tests.rb
@@ -1,6 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'HTTParty'
+require './alerts.rb'
+require './astronomy.rb'
+require './conditions.rb'
+require './current_hurricane.rb'
+require './forecast.rb'
+require './ten_day_forecast.rb'
 # require 'execute'
 
 class WeatherUnderground < Minitest::Test
