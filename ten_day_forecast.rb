29972dbfdc406ddcdb79a9332fe7fc913944dbfa
Daniel Buompane <danielbuompane@gmail.com>
2016-02-22 15:15:24 -0500

    Initialize TenDayForecast class

--- ten_day_forecast.rb
+++ ten_day_forecast.rb
@@ -1,2 +1,5 @@
 class TenDayForecast
+  def initialize(zipcode)
+    @ten_day = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/forecast10day/q/#{zipcode}.json")
+  end
 end
