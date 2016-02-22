445a313ecc7fd2286c1f10e87472733119a736b1
Daniel Buompane <danielbuompane@gmail.com>
2016-02-22 15:12:01 -0500

    Initialize CurrentHurricane class

--- current_hurricane.rb
+++ current_hurricane.rb
@@ -1,2 +1,5 @@
 class CurrentHurricane
+  def initialize(zipcode)
+    @hurricane = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/currenthurricane/q/#{zipcode}.json")
+  end
 end
