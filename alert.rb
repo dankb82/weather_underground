2366d34b58882de14ac3612b367b5c6841bb81ea
Daniel Buompane <danielbuompane@gmail.com>
2016-02-22 16:34:42 -0500

    Create json file for alerts

--- alert.rb
+++ alert.rb
@@ -1,10 +1,9 @@
 class Alerts
-  attr_accessor :alerts
   def initialize(zipcode)
     @alerts = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/alerts/q/#{zipcode}.json")
   end
 
   def get_alerts
-    
+
   end
 end
--- condition.rb
+++ condition.rb
@@ -1,4 +1,4 @@
-class Conditions
+class Condition
   def initialize(zipcode)
     @conditions = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/conditions/q/#{zipcode}.json")
   end
