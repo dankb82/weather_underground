b8b7c2ba615ca8da9ba14714b1532f23aa6cddd7
Daniel Buompane <danielbuompane@gmail.com>
2016-02-22 23:18:14 -0500

    Pass current hurricane test

--- current_hurricane.rb
+++ current_hurricane.rb
@@ -2,4 +2,12 @@ class CurrentHurricane
   def initialize(zipcode)
     @hurricane = HTTParty.get("http://api.wunderground.com/api/#{ENV["WUNDERGROUND_KEY"]}/currenthurricane/q/#{zipcode}.json")
   end
+
+  def current_hurricanes
+    hurricanes = []
+    @hurricane["currenthurricane"].each do |h|
+      hurricanes << h["stormInfo"]["stormName_Nice"]
+    end
+    hurricanes
+  end
 end
